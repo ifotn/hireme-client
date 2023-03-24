3a498653b854f0bc2083ae3375ff72b86d916db3
ifotn <rich@infrontofthenet.com>
2023-03-24 17:43:17 -0400

    l10/3 - create

--- src/app/employer/employer.component.ts
+++ src/app/employer/employer.component.ts
@@ -12,6 +12,11 @@ export class EmployerComponent implements OnInit {
   constructor(private service: EmployerService) {}
 
   employers: any;
+  name: string | undefined;
+  city: string | undefined;
+  contactEmail: string | undefined;
+  comments: string | undefined;
+  employees: number | undefined;
 
   // use service to retrieve json data from api
   getEmployers(): void {
@@ -20,6 +25,30 @@ export class EmployerComponent implements OnInit {
     })
   }
 
+  addEmployer(): void {
+    // populate new employer 
+    let employer = {
+      name: this.name,
+      city: this.city,
+      contactEmail: this.contactEmail,
+      comments: this.comments,
+      employees: this.employees
+    };
+
+    // send it to the service which posts it to the api
+    this.service.addEmployer(employer).subscribe(response => {
+      this.getEmployers();
+      this.reset();
+    });
+  }
+
+  reset(): void {
+    this.name = undefined;
+    this.city = undefined;
+    this.contactEmail = undefined;
+    this.comments = undefined;
+    this.employees = undefined;
+  }
   ngOnInit(): void {
     this.getEmployers();
   }
