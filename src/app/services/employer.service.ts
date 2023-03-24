4bdd55d23d4909c355eb6d3c8a4531b6674ef15c
ifotn <rich@infrontofthenet.com>
2023-03-24 16:47:29 -0400

    l10/2 read data from api

---
+++ src/app/services/employer.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+// refs to make http calls to server api
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployerService {
+
+  serverUrl: string = environment.serverUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getEmployers() {
+    return this.http.get(`${this.serverUrl}/api/employers`)
+  }
+}
